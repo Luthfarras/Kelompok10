26a447618bf92eff24570b93b9ae87f743d61b8e
Luthfarras <luthfarras_tresanizega_27rpl@student.smktelkom-mlg.sch.id>
2022-10-03 23:55:50 +0700

    delete minor files

--- cadangan/delete_bk.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config.php';
-$id = $_GET['id_buku'];
-mysqli_query($conn, "DELETE FROM buku WHERE id_buku='$id'");
-header("location:data_bk.php");
- ?>
--- cadangan/delete_sw.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config.php';
-$id = $_GET['nis'];
-mysqli_query($conn, "DELETE FROM siswa WHERE nis='$id'");
-header("location:data_sw.php");
- ?>
